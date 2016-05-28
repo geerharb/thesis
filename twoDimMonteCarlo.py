cb441c6b63a18b8d2d5d02dc94b5a1a9f1622a50
JR <billygeerhartiii@gmail.com>
2016-05-28 15:29:55 -0700

    update on 2d graphs

--- twoDimMonteCarlo.py
+++ twoDimMonteCarlo.py
@@ -2,9 +2,12 @@ from __future__ import division
 import numpy as np
 from math import sin,cos,tan
 import pylab as plt
+from random import randint
+import random
 degree30=30*np.pi/180.0
 class Simulator:
-	def __init__(self,nxMax,nyMax,temperature):
+	def __init__(self,nxMax,nyMax,temperature,boxScale=1.0):
+		self.boxScale=boxScale
 		hexss=[]
 		alpha=4
 		self.nxMax=nxMax
@@ -31,7 +34,62 @@ class Simulator:
 					hexss[ny][nx].density=self.liquidDensity
 		self.hexss=hexss
 		return
+	def validIndex(self,nx,ny):
+		while nx<0:
+			ny-=int(self.nxMax/2)
+			nx+=self.nxMax
+		while nx>=self.nxMax:
+			ny+=int(self.nxMax/2)
+			nx-=self.nxMax
+		while ny<0:
+			ny+=self.nyMax
+		while ny>=self.nyMax:
+			ny-=self.nyMax
+		return [nx,ny]
+	def nearestNeighbors(self,nx,ny):
+		neighbors=[]
+		neighbors.append(self.validIndex(nx+1,ny))
+		neighbors.append(self.validIndex(nx-1,ny))
+		neighbors.append(self.validIndex(nx,ny+1))
+		neighbors.append(self.validIndex(nx,ny-1))
+		neighbors.append(self.validIndex(nx-1,ny+1))
+		neighbors.append(self.validIndex(nx+1,ny-1))
+		return neighbors
+	def energy(self,nx,ny):
+		L=2*(3**0.5)/3*self.boxScale
+		index=validIndex(nx,ny)
+		density0=hexxs[index[1]][index[0]].density
+		energyTotal=0
+		for index in nearestNeighbors(nx,ny):
+			density1=hexxs[index[1]][index[0]].density
+			energyTotal+=abs(density1-density0)*L
+		return energyTotal
+	
+	def simulate(self,validMoves=1000,maxMoves=1000000):
+		while validMoves>0 and maxMoves>0:
+			maxMoves-=1
+			nx1=randint(0,self.nxMax)
+			ny1=randint(0,self.nyMax)
+			neighbor=nearestNeighbors(nx1,nx2)
+			neighbor=neighbor[randint(0,len(neighbor))]
+			nx2=neighbor[0]
+			ny2=neighbor[1]
+			if abs(hexss[ny1][nx1]-hexss[ny2][nx2])<1e-7: continue
+			energyBefore=energy(nx1,ny1)+energy(nx2,ny2)
+			hexss[ny1][nx1],hexss[ny2][nx2]=hexss[ny2][nx2],hexss[ny1][nx1]
+			energyAfter=energy(nx1,ny1)+energy(nx2,ny2)
+			if energyAfter<energyBefore:
+				validMoves-=1
+				continue
+			acceptanceRate=np.exp(-(energyAfter-energyBefore)/self.temperature)
+			if random.uniform(0,1.0)<acceptanceRate: 
+				validMoves-=1
+				continue
+			hexss[ny1][nx1],hexss[ny2][nx2]=hexss[ny2][nx2],hexss[ny1][nx1]
+		return
 	def draw(self):
+		#for index in self.nearestNeighbors(randint(0,self.nxMax),randint(0,self.nyMax)):
+		#	self.hexss[index[1]][index[0]].color='g'
 		for ny in range(0,self.nyMax):
 			for nx in range(0,self.nxMax):
 				self.hexss[ny][nx].draw()
@@ -60,7 +118,7 @@ class Hexagon:
 		for i in range(len(xyCoord)):
 			xyCoord[i][0]+=self.x
 			xyCoord[i][1]+=self.y
-		line=plt.Polygon(xyCoord,color=self.color, edgecolor=self.color)#fill=None
+		line=plt.Polygon(xyCoord,color=self.color)#, edgecolor=self.color)#fill=None
 		plt.gca().add_line(line)
 		#xyCoord=np.array(xyCoord)
 		#x=xyCoord[:,0]
@@ -73,9 +131,10 @@ class Hexagon:
 
 
 plt.figure()
-nxMax=40
-nyMax=int(nxMax*cos(degree30))#-sin(degree30)*nxMax
+nxMax=10 #must be multiple of 2
+nyMax=int(nxMax*cos(degree30))+1#-sin(degree30)*nxMax
 box=Simulator(nxMax,nyMax,0.5)
+box.simulate(validMoves=1)
 box.draw()
 #plt.xlim((0,2*nMax+1))
 #plt.ylim((0,2*nMax+1))
